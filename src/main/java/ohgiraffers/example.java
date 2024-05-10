b05544ec4b8adf73415927084ad776a059529cd2
HEun0420 <usialeta@gmail.com>
2024-05-10 11:09:57 +0900

    출력 문자 변경

--- src/main/java/ohgiraffers/example.java
+++ src/main/java/ohgiraffers/example.java
@@ -4,7 +4,7 @@ public class example {
     public static void main(String[] args) {
 
         System.out.print("Hello World!");
-        System.out.println("한글은 안 깨져요");
+        System.out.println("졸리당...다음에는 좀 더 일찍 자야지");
 
     }
 }
